4a091cc1e4b41277ef51bc84b8c62818c999f23c
niiccoo2 <web@niiccoo2.xyz>
2025-06-01 01:46:37 -0400

    Refactor routes to use render_template and remove testing script
    Updated the index route in `app/routes.py` to utilize Flask's `render_template` for rendering the home page, enhancing maintainability. Removed commented-out print statements for cleaner code. Modified the JSON file path in `main.py` for consistency. Deleted the obsolete `testing.py` script as it is no longer needed.

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,13 @@
 from app import app
+from flask import render_template
 import main
 
 @app.route('/')
 @app.route('/index')
 def index():
     cities = main.fetch_cities_from_json()
-    print(cities)
     random_image = main.grab_random_city(cities)
-    print(random_image)
     image = random_image.get('image')
-    print(image)
+    
     user = {'username': 'Nico'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-        <img src="''' + image + '''" height="250" width="500">
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user, image=image)
