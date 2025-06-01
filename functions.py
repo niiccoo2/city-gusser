7d94d6885e01135791e940227069854c87f0c330
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-06-01 09:45:54 -0400

    Remove commented-out get_city_data function and ensure logic() is called in main block

--- functions.py
+++ functions.py
@@ -208,32 +208,5 @@ def logic():
         
         print(dnd)
 
-
-
-# def get_city_data(city, data_type):
-#     json_file_path = "./photos-database.json"
-#     try:
-#         with open(json_file_path, "r", encoding="utf-8") as file:
-#             data = json.load(file)
-#             local_list_city = data.get("cities", [])
-
-#             if data_type == "all":
-#                 return [city_data for city_data in local_list_city if city_data["city"].lower() == city.lower()]
-#             else:
-#                 for city_data in local_list_city:
-#                     if city_data["city"].lower() == city.lower():
-#                         print(city_data.get(data_type))
-#                         return city_data.get(data_type)
-#                 print(f"{city} not found or {data_type} not available.")
-#                 return None
-#     except FileNotFoundError:
-#         print(f"Error: File not found at {json_file_path}")
-#         return []
-#     except json.JSONDecodeError:
-#         print("Error: Failed to decode JSON file.")
-#         return []
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#         return []
-
-logic()
+if __name__ == "__main__":
+    logic()
