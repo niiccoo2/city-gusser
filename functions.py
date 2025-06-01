572821d83f11028c9add42d98bbcaf7352eb604c
Babychicken11 <xvrowens@gmail.com>
2025-06-01 08:57:54 -0400

    nico really told me

--- functions.py
+++ functions.py
@@ -1,17 +1,18 @@
 from openai import OpenAI
 import os
 from main import *
-from api import *
+
 
 def get_summary(city):
-    client = OpenAI(api_key=get_openai_key())
+    with open("r", "Api.txt") as api:
+        client = OpenAI(api)
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": f"Write a one paragraph summary of {city}. It is for a city gussing game so you can't give away the city is. Maybe some fun facts. Do not give any locations."},
-        ],
-        temperature=0,
-    )
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": f"Write a one paragraph summary of {city}. It is for a city gussing game so you can't give away the city is. Maybe some fun facts. Do not give any locations."},
+            ],
+            temperature=0,
+        )
 
-    return response.choices[0].message.content
+        return response.choices[0].message.content
