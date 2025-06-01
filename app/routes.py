1bd53f5b3aae6fc7725db6aeea3973e0de105cce
niiccoo2 <web@niiccoo2.xyz>
2025-05-31 23:30:10 -0400

    Enhance index route with HTML response and user greeting
    Update the index route to return an HTML page instead of plain text. Introduce a user dictionary to personalize the greeting with a username. Import the json module for potential future use. Add a placeholder for an image in the HTML structure. Include a new .flaskenv file in the project.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    
+    user = {'username': 'Nico'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+        <img src="''''''" height="250" width="500">
+    </body>
+</html>'''
