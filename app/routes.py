9d3a7cc333889970fc406afcc3cecdf0c1867ac2
niiccoo2 <web@niiccoo2.xyz>
2025-06-01 15:07:41 -0400

    Implement city guessing feature in index route and template
    Refactor LoginForm to support city guessing by replacing login fields with a guess field. Update index route to handle form submission and display guesses. Modify index.html to include a form for city guessing and adjust table headers for clarity. Add initial state management for guesses.

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,34 @@ from app import app
 from flask import render_template
 from functions import *
 from app.forms import LoginForm
+state = {
+    'city': 'las vagas',
+    'guesses': []
+}
 
 @app.route('/')
 @app.route('/index')
 def index():
+    form = LoginForm()
     city = pick_random_city()
     image = city[0].get('image')
     blurb = get_summary(f"{city[0].get('city')}, {city[0].get('country')}")
-    user = {'username': 'Nico'}
-    return render_template('index.html', title='Home', image=image, blurb=blurb)
 
-@app.route('/login')
+    return render_template('index.html', title='Home', image=image, blurb=blurb, form=form, state=state)
+
+from flask import render_template, flash, redirect
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        # flash('User guessed: {}.'.format(
+        #     form.guess.data))
+        print(form.guess.data)
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+# @app.route('/login')
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', title='Sign In', form=form)
